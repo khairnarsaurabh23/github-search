9a27e54b59415153761f735573703cce743cc971
Khairnar Saurabh Ravindra <srkhairnar23@gmail.com>
2021-12-27 15:11:05 +0530

    firebase update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+  apiKey: "AIzaSyC7Ga4o5qu6bcufUZtG5W2ZXjZ3VVZA-JI",
+  authDomain: "github-search-80eab.firebaseapp.com",
+  projectId: "github-search-80eab",
+  storageBucket: "github-search-80eab.appspot.com",
+  messagingSenderId: "523786834902",
+  appId: "1:523786834902:web:01d272ba00824011324a77",
+  measurementId: "G-B6VPB7H78X"
+  },
 };
