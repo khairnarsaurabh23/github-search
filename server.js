75315f30e22247ceb1189c20519448149c21eff6
Khairnar Saurabh Ravindra <srkhairnar23@gmail.com>
2021-12-27 15:30:13 +0530

    server.js update

--- server.js
+++ server.js
@@ -1,17 +1,13 @@
-//server for production
-//Install express server
+//configure server for heroku deploy
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
-
+app.use(express.static(__dirname + '/dist/github-login'));
 app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/index.html'));
+	res.sendFile(path.join(__dirname+'/dist/github-login/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
+//set the port
 app.listen(process.env.PORT || 8080);
